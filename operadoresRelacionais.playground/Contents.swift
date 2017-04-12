eb3bc8fb402d1c6f6dd604e6e04c0a3dd931a63b
Samantha Correa <samanthacorrea@iMAC-Sec-3.local>
2017-04-12 13:51:51 -0400

    playground

--- operadoresRelacionais.playground/Contents.swift
+++ operadoresRelacionais.playground/Contents.swift
@@ -2,4 +2,25 @@
 
 import UIKit
 
-var str = "Hello, playground"
+func multiplicar(a: Int, b: Int) -> Int{
+    return a * b
+}
+
+var a: Int
+var b: Int
+
+a = 5
+b = 5
+
+//print(multiplicar(a: a, b: a))
+
+func idade(anoNasc: Int) -> Int{
+    return 2017 - anoNasc
+}
+
+print(idade(anoNasc: 1996))
+
+var sam = "sam"
+//print(sam)
+
+var teste: Int
