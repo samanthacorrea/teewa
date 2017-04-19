2bd7ec20e2265b67ac1764f87f1e6abcc5be225b
Samantha Correa <scl@icomp.ufam.edu.br>
2017-04-19 06:13:55 -0400

    atualizacoes do projetinho de curso de ios

--- Alerta/Alerta/ViewController.swift
+++ Alerta/Alerta/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //criação de pop-up
+    @IBAction func abrirAlerta(_ sender: Any) {
+        let alertaController = UIAlertController(title: "Adicionar Contato", message: "Deseja adicionar o contato?", preferredStyle: .actionSheet)
+        
+        let confirmar = UIAlertAction(title: "ok", style: .default, handler: nil)
+        let cancelar = UIAlertAction(title: "cancel", style: .destructive, handler: nil)
+
+        alertaController.addAction(confirmar)
+        alertaController.addAction(cancelar)
+        present(alertaController, animated: true, completion: nil)
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
